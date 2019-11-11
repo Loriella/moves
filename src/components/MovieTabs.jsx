d1a20d29836f948f2918ed055b078ab217a961db
Loriella <loris.orlova@gmail.com>
2019-11-11 17:08:29 +0300

    refactoring MovieTabs component

--- src/components/MovieTabs.jsx
+++ src/components/MovieTabs.jsx
@@ -2,40 +2,36 @@ import React from 'react';
 
 const MovieTabs = props => {
   const { sort_by, updateSortBy } = props;
+  const handleClick = value => () => {
+      updateSortBy(value);
+  };
+
+  const getClassLink = (value) => {
+    return `nav-link ${sort_by === value ? "active" : ""}`
+  };
+
   return (
     <ul className="tabs nav nav-pills">
       <li className="nav-item">
         <div
-          className={`nav-link ${
-            sort_by === "popularity.desc" ? "active" : ""
-          }`}
-          onClick={() => {
-            updateSortBy("popularity.desc")
-          }}
+          className={getClassLink("popularity.desc")}
+          onClick={handleClick("popularity.desc")}
         >
           Popularity desc
         </div>
       </li>
       <li className="nav-item">
         <div
-          className={`nav-link ${
-            sort_by === "revenue.desc" ? "active" : ""
-          }`}
-          onClick={() => {
-            updateSortBy("revenue.desc")
-          }}
+          className={getClassLink("revenue.desc")}
+          onClick={handleClick("revenue.desc")}
         >
           Revenue desc
         </div>
       </li>
       <li className="nav-item">
         <div
-          className={`nav-link ${
-            sort_by === "vote_average.desc" ? "active" : ""
-          }`}
-          onClick={() => {
-            updateSortBy("vote_average.desc")
-          }}
+          className={getClassLink("vote_average.desc")}
+          onClick={handleClick("vote_average.desc")}
         >
           Vote average desc
         </div>

a35686b5b7931e3a1d856695961a617ed9dfb251
Loriella <loris.orlova@gmail.com>
2019-11-15 09:48:46 +0300

    add functionality in the component LoginForm

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,11 +1,59 @@
 import React from 'react';
+import {API_URL, API_KEY_3} from '../utils/api';
 
 class LoginForm extends React.Component {
+  constructor() {
+    super();
+
+    this.inputNameRef = React.createRef();
+
+    this.state = {
+      username: "",
+      password: ""
+    }
+  }
+
+  componentDidMount() {
+    this.inputNameRef.current.focus();
+  }
+
+  // onChangeUserName = event => {
+  //   this.setState({
+  //     username: event.target.value
+  //   })
+  // };
+  //
+  // onChangePassword = event => {
+  //   this.setState({
+  //     password: event.target.value
+  //   })
+  // };
+
+  onChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  };
+
+  onSubmit = event => {
+    event.preventDefault();
+
+    fetch(`${API_URL}/authentication/token/new?api_key=${API_KEY_3}`)
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+
+    })
+  };
 
   render() {
     return (
       <div className="form-login-container">
-        <form className="form-login">
+        <form
+          className="form-login"
+          onSubmit={this.onSubmit}
+        >
           <h1 className="h3 mb-3 font-weight-normal text-center">
             Авторизация
           </h1>
@@ -17,6 +65,9 @@ class LoginForm extends React.Component {
               id="username"
               placeholder="Пользователь"
               name="username"
+              value={this.state.username}
+              ref={this.inputNameRef}
+              onChange={this.onChange}
             />
           </div>
           <div className="form-group">
@@ -27,6 +78,8 @@ class LoginForm extends React.Component {
               id="password"
               placeholder="Пароль"
               name="password"
+              value={this.state.password}
+              onChange={this.onChange}
             />
           </div>
           <button
